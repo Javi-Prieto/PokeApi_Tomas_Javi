e1655090d92e1d8f5ef14e489a5d2381642fe67b
tomasdelpinorep <tomasdelpino28@gmail.com>
2023-10-03 12:02:29 +0200

    itemsPage finished, but ugly af

--- js/itemsPage.js
+++ js/itemsPage.js
@@ -4,15 +4,34 @@ $(document).ready(function () {
         type: 'GET'
     }).done(function (resp) {
         var itemList = resp.results;
-        itemList.forEach(item => {
-            var template = `
-            <tr>
-                <td>${item.name}</td>
-                <td></td>
-                <td></td>
-            </tr>`;
 
-            $("#tableBody").append(template);
+        itemList.forEach(item => { //Need to search for the specific items to get 
+            //access to all the attributes
+            $.ajax({
+                url: `${item.url}`,
+                type: 'GET'
+            }).done(function (fullItem) {
+
+                var template = `
+                <tr>
+                    <td>${fullItem.names[7].name}</td>
+                    <td>${fullItem.category.name}</td>
+                    <td>${fullItem.effect_entries[0].effect}</td>
+                </tr>`;
+
+                $("#tableBody").append(template);
+            });
         });
     })
+
+    $(document).on('click', '#searchBtn', function () {
+        $('#collapseSearch').show();
+        $('#searchBtn').hide();
+        $('#xClose').show();
+    });
+    $(document).on('click', '.closeSearch', function () {
+        $('#collapseSearch').hide();
+        $('#searchBtn').show();
+        $('#xClose').hide();
+    });
 });
