d35cfe187bc15a0a9ff5ee10ae3bec19a47978fb
Javier Prieto <jpojaviprior@gmail.com>
2023-10-04 19:09:16 +0200

    finished except from the adapt to dispositives

--- js/typePage.js
+++ js/typePage.js
@@ -1,4 +1,47 @@
 $(document).ready(function () {
+    const itemCount = 18;
+    const circleContainer = $("#circle-container");
+    const radius = 357; 
+    const angleStep = (2 * Math.PI) / itemCount;
+
+    
+    for (var i = 0; i < itemCount; i++) {
+        const x = radius * Math.cos(i * angleStep) + radius;
+        const y = radius * Math.sin(i * angleStep) + radius;
+
+        var item = `
+            <button class = "circle-item btn" style= "top:${y}px; left:${x}px;">
+                <img src="../img/${i+1}.png" alt="" />
+            </button>
+        `;
+        circleContainer.hide();
+        circleContainer.append(item);
+    } 
+   
+    var midScreenHCircle = ($('main').width() / 2) - (radius + 20);
+    var midScreenVCircle  = ($('main').height() / 2) - (radius + 82);
+    var topCircle = midScreenVCircle + 'px';
+    var leftCircle = midScreenHCircle + 'px';
+
+
+    var midScreenHPokeball = ($('main').width() / 2) - ($('#showTypes>img').width()/2);
+    var midScreenVPokeball  = ($('main').height() / 2) - ($('#showTypes>img').height()/2 - 44);
+    var topPokeball = midScreenVPokeball + 'px';
+    var leftPokeball = midScreenHPokeball + 'px';
+
+    circleContainer.css({
+        'top': topCircle,
+        'left': leftCircle
+    });
+    $('#showTypes').css({
+        'top': topPokeball,
+        'left': leftPokeball
+    });
+    $('#closeTypes').css({
+        'top': topPokeball,
+        'left': leftPokeball
+    });
+    
     $(document).on('click', '#searchBtn', function () {
         $('#collapseSearch').show();
         $('#searchBtn').hide();
@@ -11,24 +54,8 @@ $(document).ready(function () {
     });
 
 
-        const itemCount = 18;
-        const circleContainer = $("#circle-container");
-        const radius = 360; 
-        const angleStep = (2 * Math.PI) / itemCount;
+        
 
-        for (var i = 0; i < itemCount; i++) {
-            const x = radius * Math.cos(i * angleStep) + radius;
-            const y = radius * Math.sin(i * angleStep) + radius;
-
-            var item = `
-                <button class = "circle-item btn" style= "top:${y}px; left:${x}px;">
-                    <img src="../img/${i+1}.png" alt="" />
-                </button>
-
-            `;
-            circleContainer.hide();
-            circleContainer.append(item);
-        }
     $(document).on('click', '#showTypes', function () {
         circleContainer.show();
         $('#showTypes').hide();
