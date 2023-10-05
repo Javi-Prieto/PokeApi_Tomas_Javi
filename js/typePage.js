73927632b548d3374a8fd9978a74c8510d8132f9
Javier Prieto <jpojaviprior@gmail.com>
2023-10-05 13:27:39 +0200

    adapting the page for small pages

--- js/typePage.js
+++ js/typePage.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
+    
+    
     const itemCount = 18;
     const circleContainer = $("#circle-container");
     const radius = 357; 
     const angleStep = (2 * Math.PI) / itemCount;
+    var showTypeToggle = true;
 
     
     for (var i = 0; i < itemCount; i++) {
@@ -14,9 +17,10 @@ $(document).ready(function () {
                 <img src="../img/${i+1}.png" alt="" />
             </button>
         `;
-        circleContainer.hide();
+        
         circleContainer.append(item);
     } 
+
    
     var midScreenHCircle = ($('main').width() / 2) - (radius + 20);
     var midScreenVCircle  = ($('main').height() / 2) - (radius + 82);
@@ -41,6 +45,17 @@ $(document).ready(function () {
         'top': topPokeball,
         'left': leftPokeball
     });
+    $(document).on('click', '#showTypes', function () {
+        if(showTypeToggle){
+            circleContainer.show().fadeOut();
+            showTypeToggle = false;
+        }else{
+            circleContainer.hide().fadeIn();
+            showTypeToggle = true;
+        }
+        
+    });
+   
     
     $(document).on('click', '#searchBtn', function () {
         $('#collapseSearch').show();
@@ -53,17 +68,24 @@ $(document).ready(function () {
         $('#xClose').hide();
     });
 
-
+    $.ajax({
+        type: "GET",
+        url: "https://pokeapi.co/api/v2/type/"
+    }).done(resp => {
+        var types = resp.results;
+        types.forEach(type => {
+            var template = 
+            `
+            <div class="col-lg-3 col-md-3 col-sm-6 border border-warning row bg-light">
+                <div class="col-6"><img src="../img/1.png" alt="" /></div>
+                <div class="col-6"><h6 class="h6">${type.name}</h6></div>
+            </div>
+            `;
+            $('#typesList').append(template);
+        });
         
+    });
+    
 
-    $(document).on('click', '#showTypes', function () {
-        circleContainer.show();
-        $('#showTypes').hide();
-        $('#closeTypes').show();
-    });
-    $(document).on('click', '#closeTypes', function () {
-        circleContainer.hide();
-        $('#showTypes').show();
-        $('#closeTypes').hide();
-    });
+    
 });
